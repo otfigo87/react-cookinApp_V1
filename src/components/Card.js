b2c717b69c162539d31f5339a146473e42029c82
Otmane Aatik <aaotmane@gmail.com>
2023-01-27 17:53:39 -0500

    css

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,10 @@ import React from "react";
 const Card = ({ meal }) => {
   return (
     <div className="meal-card">
-      <h2>{meal.strMeal}</h2>
-      <p>Origin: {meal.strArea}</p>
-      <img src={meal.strMealThumb} alt={"photo " + meal.strMeal} />
-      <p>{meal.strInstructions}</p>
+        <h2>{meal.strMeal}</h2>
+        <p>Origin: {meal.strArea}</p>
+        <img src={meal.strMealThumb} alt={"photo " + meal.strMeal} />
+        <p>{meal.strInstructions}</p>
     </div>
   );
 };
